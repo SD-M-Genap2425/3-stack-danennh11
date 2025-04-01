2b66f61b53e2cae078166715d7301fadf442ac61
meramii1 <danendranayott@gmail.com>
2025-04-01 10:30:38 +0700

    Tugas Stack 5053241032

--- src/Solution/Solution/Program.cs
+++ src/Solution/Solution/Program.cs
@@ -9,16 +9,25 @@ internal class Program
     static void Main(string[] args)
     {
         // Browser history
-        
-        
+        var historyManager = new HistoryManager();
+            historyManager.KunjungiHalaman("google.com");
+            historyManager.KunjungiHalaman("example.com");
+        var historyManager1 = new HistoryManager();
+            historyManager1.KunjungiHalaman("google.com");
+            historyManager1.KunjungiHalaman("example.com");
+            var previousPage = historyManager1.Kembali();
         
         // Bracket validator
+        var validator = new BracketValidator();
+        var expression = "{[()]}";
+        bool isValid = validator.ValidasiEkspresi(expression);
 
         
 
         //Palindrome Checker
+        var input = "Kasur ini rusak";
+        var result = PalindromeChecker.CekPalindrom(input);
 
-        
 
     }
 }
